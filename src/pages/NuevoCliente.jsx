897279f351c6f6c4441c6a0845b4285a353540ea
Fernando Schwalb Costa <fschwalb@gmail.com>
2023-03-26 20:04:21 +0200

    Formulario.jsx created

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,4 +1,9 @@
-import { useNavigate } from "react-router-dom";
+import { Form, useNavigate } from "react-router-dom";
+import Formulario from "../components/Formulario";
+
+export function action() {
+    console.log('submit al Form...');
+}
 
 
 function NuevoCliente() {
@@ -19,8 +24,19 @@ function NuevoCliente() {
                 </button>
             </div>
 
-            <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
-                <p>Formulario aqui</p>
+            <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
+                <Form
+                    method='post'
+                >
+                    <Formulario />
+
+                    <input 
+                        type="submit"
+                        className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
+                        value='Registrar Cliente'
+                    />
+                </Form>
+
             </div>
         </>
     )
